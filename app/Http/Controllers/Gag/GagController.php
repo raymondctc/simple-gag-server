aec03ca2502d52a958b04364630c5a252a700123
Raymond Chan <raymond@9gag.com>
2016-01-14 19:39:30 +0800

    init

---
+++ app/Http/Controllers/Gag/GagController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Gag;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * Created by PhpStorm.
+ * User: raymond
+ * Date: 14/1/2016
+ * Time: 7:16 PM
+ */
+class GagController extends Controller
+{
+
+    public function show($page)
+    {
+        return"";
+    }
+}
