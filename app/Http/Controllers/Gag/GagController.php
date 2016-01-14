df943e752f908af37a13c8cb079459371db31785
Raymond Chan <raymond@9gag.com>
2016-01-14 20:15:59 +0800

    simple api

--- app/Http/Controllers/Gag/GagController.php
+++ app/Http/Controllers/Gag/GagController.php
@@ -13,8 +13,69 @@ use App\Http\Controllers\Controller;
 class GagController extends Controller
 {
 
+    const LIMIT = 10;
+
     public function show($page)
     {
-        return"";
+        $limit = 10;
+        $offset = $page * $limit;
+        $nextOffset = ($page + 1)* $limit;
+        $count = \DB::table("GAG_ITEM")->where('TYPE', '=', 'Photo')->orWhere('TYPE', '=', 'Animated')->count();
+        $results = \DB::table("GAG_ITEM")->where('TYPE', '=', 'Photo')->orWhere('TYPE', '=', 'Animated')->limit($limit)->offset($offset)->get();
+
+        srand(time());
+        $randValue = (float) rand() / (float)getrandmax();
+
+        if ($randValue >= 0.15) {
+            $response = array(
+                "meta" => array(
+                    "code" => 200,
+                ),
+                "data" => array(
+                    "gags" => self::_tmpResultConverter($results),
+                    "has_next"  => ($nextOffset >= $count) ? false : true,
+                    "next_page" => ($nextOffset >= $count) ? null : $page + 1 . "",
+                )
+            );
+        } else {
+            $response = array(
+                "meta" => array(
+                    "code" => 404
+                )
+            );
+        }
+
+
+
+//        return (json_encode($results));
+
+
+
+        return json_encode($response);
+    }
+
+    // Photo/Animated
+    private static function _tmpResultConverter($results)
+    {
+        $convertedResults = array();
+
+        foreach ($results as $result) {
+            $converted = array();
+            $converted['_id'] = $result->{'_id'};
+            $converted['title'] = $result->{'TITLE'};
+            $converted['type'] = $result->{'TYPE'};
+
+            $gagMedia = json_decode($result->{'GAG_MEDIA_JSON'}, true);
+
+            $converted['url'] = $gagMedia['image700']['url'];
+            $converted['width'] = $gagMedia['image700']['width'];
+            $converted['height'] = $gagMedia['image700']['height'];
+
+            $converted['nsfw'] = $result->{'NSFW'};
+
+            $convertedResults[] = $converted;
+        }
+
+        return $convertedResults;
     }
 }
